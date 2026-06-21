59e077d5aef661bf026ffd22641672ee7d8442dd
Chenjv2 <chenjv2.vcbs@gmail.com>
2026-06-21 17:54:30 +0200

    added joke command

--- index.js
+++ index.js
@@ -29,14 +29,6 @@ app.command("/brokenbot-help", async ({ ack, respond }) => {
 /brokenbot-catfact - Get a cat fact`,
   });
 });
-app.command("/brokenbot-help", async ({ ack, respond }) => {
-  await ack();
-  await respond({
-    text: `Available Commands:
-/brokenbot-ping - Check bot latency
-/brokenbot-catfact - Get a cat fact`,
-  });
-});
 app.command("/brokenbot-catfact", async ({ ack, respond }) => {
   await ack();
 
@@ -47,3 +39,12 @@ app.command("/brokenbot-catfact", async ({ ack, respond }) => {
     await respond({ text: "Failed to fetch a cat fact." });
   }
 });
+app.command("/brokenbot-joke", async ({ ack, respond }) => {
+  await ack();
+  try {
+    const response = await axios.get("https://official-joke-api.appspot.com/random_joke");
+    await respond({ text: `Here's a joke for you:\n${response.data.setup}\n${response.data.punchline}` });
+  } catch (err) {
+    await respond({ text: "Failed to fetch a joke." });
+  };
+}); 
